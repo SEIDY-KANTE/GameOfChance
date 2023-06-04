d23ff690368e8fcc843c0db9586b85956e7efd89
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-06-04 04:15:19 +0300

    Implementation of Game Strucutre

--- src/Game.c
+++ src/Game.c
@@ -26,6 +26,46 @@ Game new_Game(){
 	this->delete_Game=&delete_Game;
 	
 }
+
+void run(const Game this){
+	char* personFile="Person.txt";
+	char* numbersFile="Numbers.txt";
+	File inMyFile = new_File(personFile,numbersFile);
+
+	int numberOfPerson=inMyFile->numberOfPerson;
+	int roundNumber=inMyFile->roundNumber;
+
+	inMyFile->readPersonFile(inMyFile, personFile);
+	inMyFile->readNumbersFile(inMyFile, numbersFile);
+
+	
+	int numberOfPlayer=inMyFile->numberOfPerson;
+	int losersPerRound=0;
+
+	int i=0;
+	boolean isGameOver;
+
+	for(i; i<roundNumber; i++){
+		int j=0;
+		
+		isGameOver = true;
+		for(j; j<numberOfPerson; j++)
+		{			
+			if(inMyFile->person[j]->money>=1000.0){
+				inMyFile->person[j]->play(inMyFile->person[j],this,inMyFile->numbers[i]);
+				//Here Find the richest person
+				isGameOver=false;
+			}
+			
+		}
+
+		if(isGameOver) break;
+		
+		draw(inMyFile->numbers[i],i+1,this->tableBalance,this->theRichestPerson,this->maxBalance);
+	}	
+	//drawGameOver(i,this->tableBalance);
+}
+
 void delete_Game(const Game this){
 	if(this==NULL) return;
 	free(this);
